017461b0e0fc307e53019bb4570885a283ddafc3
awe <aweful@awews.com>
2025-05-25 16:27:18 -0400

    add StudySession Model with virtuals

---
+++ models/StudySession.ts
@@ -0,0 +1,58 @@
+// models/StudySession.ts
+/*
+https://mongoosejs.com/docs/tutorials/virtuals.html
+add virtuals to json export
+do blog post on virtuals
+**/
+import mongoose, { Schema, Document } from 'mongoose';
+
+// const opts = { toJSON: { virtuals: true } };
+
+export interface IStudySession extends Document {
+  userId: mongoose.Types.ObjectId;
+  listId: mongoose.Types.ObjectId;
+  startTime: Date;
+  endTime?: Date;
+  totalCards: number;
+  correctCount: number;
+  incorrectCount: number;
+  completedCards: number;
+  
+  // Virtual properties
+  isComplete: boolean;
+  accuracy: number;
+  durationSeconds: number;
+}
+
+const StudySessionSchema: Schema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    listId: { type: Schema.Types.ObjectId, ref: 'List', required: true },
+    startTime: { type: Date, default: Date.now },
+    endTime: { type: Date },
+    totalCards: { type: Number, default: 0 },
+    correctCount: { type: Number, default: 0 },
+    incorrectCount: { type: Number, default: 0 },
+    completedCards: { type: Number, default: 0 }
+  },
+  { timestamps: true }
+);
+
+// Virtual property to check if session is complete
+StudySessionSchema.virtual('isComplete').get(function() {
+  return this.completedCards >= this.totalCards;
+});
+
+// Virtual property to calculate accuracy
+StudySessionSchema.virtual('accuracy').get(function() {
+  const answered = this.correctCount + this.incorrectCount;
+  return answered > 0 ? (this.correctCount / answered) * 100 : 0;
+});
+
+// Virtual property to calculate duration in seconds
+StudySessionSchema.virtual('durationSeconds').get(function() {
+  if (!this.endTime) return 0;
+  return Math.round((this.endTime.getTime() - this.startTime.getTime()) / 1000);
+});
+
+export default mongoose.models.StudySession || mongoose.model<IStudySession>('StudySession', StudySessionSchema);
