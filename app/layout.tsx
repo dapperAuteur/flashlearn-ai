79da5fa26b0afea528e6a376e4ecd334a4b3be54
awe <aweful@awews.com>
2025-09-24 08:32:26 -0400

    WIP on feat/pdf-generate
    moved layout outside of provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import PublicHeader from '@/components/layout/PublicHeader';
 import { usePathname } from 'next/navigation';
 import '@/lib/services/syncService';
 import { StudySessionProvider } from '@/contexts/StudySessionContext';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <AuthProvider>
           <StudySessionProvider>
             {showPublicHeader && <PublicHeader />}
