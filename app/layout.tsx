80644db9479b59fb865f4231341f818d052a0d86
awe <aweful@awews.com>
2025-09-24 07:50:40 -0400

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import PublicHeader from '@/components/layout/PublicHeader';
 import { usePathname } from 'next/navigation';
 import '@/lib/services/syncService';
 import { StudySessionProvider } from '@/contexts/StudySessionContext';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>
+          <Analytics />
           <StudySessionProvider>
             {showPublicHeader && <PublicHeader />}
             {children}
