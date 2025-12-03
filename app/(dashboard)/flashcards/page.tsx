5da0c91ea62d4340325c5abdd2d8d401f2253121
awe <aweful@awews.com>
2025-12-03 14:17:08 -0500

    remove 'no explicit any' eslint ignore

--- app/(dashboard)/flashcards/page.tsx
+++ app/(dashboard)/flashcards/page.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+ /* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
 import { useEffect, useState, useCallback } from 'react';
